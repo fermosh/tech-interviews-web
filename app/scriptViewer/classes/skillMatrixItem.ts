feb1fdeeaec08110c5db1859f6bd3b160966f986
Victor Parra <vmanuel.parra@gmail.com>
2017-02-21 16:19:58 -0600

    Clean

--- app/scriptViewer/classes/skillMatrixItem.ts
+++ app/scriptViewer/classes/skillMatrixItem.ts
@@ -1,20 +1,13 @@
-/* Defines the skill entity */
-import { ITopic } from '../interfaces/topic';
-import { IQuestion } from '../interfaces/question';
-import { IExercise } from '../interfaces/exercise';
-
 export class SkillMatrixItem {
     id: number;
-    parentId: number;
     name: string;
     isSelected: boolean;
     className: string;
     skillLevel: number;
     hasChilds: boolean;
 
-    constructor(id: number, parentId: number, name: string, skillLevel: number, hasChilds: boolean) {
+    constructor(id: number, name: string, skillLevel: number, hasChilds: boolean) {
         this.id = id;
-        this.parentId = parentId;
         this.name = name;
         this.skillLevel = skillLevel;
         this.hasChilds = hasChilds;
@@ -22,15 +15,15 @@ export class SkillMatrixItem {
         this.className = this.getClassName();
     }
 
-    getClassName(): string {
-        if (this.skillLevel == 1) {
-            return "treegrid-parent";
+    getClassName(): string
+    {
+        if (this.skillLevel === 1) {
+            return 'treegrid-parent';
         }
         if (this.hasChilds) {
-            return "treegrid-parent treegrid-child";
+            return 'treegrid-parent treegrid-child';
         }
-        else {
-            return "treegrid-child";
-        }
+
+        return 'treegrid-child';
     }
 }
