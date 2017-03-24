39d9f2c3f15bcf5755c133cbffd6021cae10e517
Victor Parra <victor_parra@epam.com>
2017-03-24 12:38:07 -0600

    Manually merge differences

--- src/app/scriptViewer/script-viewer.module.ts
+++ src/app/scriptViewer/script-viewer.module.ts
@@ -5,11 +5,13 @@ import { HistoryCommentComponent } from './components/history-comment.component'
 import { ReportViewerComponent } from './components/report-viewer.component';
 import { QuestionBankFilterPipe } from './pipes/question-bank-filter.pipe';
 import { SharedModule } from './../shared/shared.module';
+import { SortablejsModule } from 'angular-sortablejs';
 import { ScriptViewerRoutingModule } from './script-viewer-routing.module';
 
 @NgModule({
     imports: [
         SharedModule,
+        SortablejsModule,
         ScriptViewerRoutingModule
     ],
     declarations: [
